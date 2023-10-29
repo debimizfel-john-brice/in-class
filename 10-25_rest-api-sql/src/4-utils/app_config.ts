258337081ff1818ee1260631cbb17ae8df27a259
deborah mizrahi <debimizfel@gmail.com>
2023-10-29 17:55:10 +0200

    .

---
+++ 10-25_rest-api-sql/src/4-utils/app_config.ts
@@ -0,0 +1,15 @@
+class AppConfig {
+    // Database host:
+    static my_sql_host = "localhost";
+
+    // Database name:
+    static my_sql_database = "northwind";
+
+    // Database user:
+    static my_sql_user = "root";
+
+    // Database password:
+    static my_sql_password = "root";
+
+}
+export default AppConfig;
