81420975f8061617058e2e200cd9c248b45de985
deborah mizrahi <debimizfel@gmail.com>
2023-11-20 19:17:02 +0200

    .

--- 11-12_northwind-fullstack/Backend/src/3-middleware/catch-all.ts
+++ 11-12_northwind-fullstack/Backend/src/3-middleware/catch-all.ts
@@ -2,25 +2,25 @@ import { NextFunction, Request, Response } from "express";
 import logger from "../4-utils/logger";
 import appConfig from "../4-utils/app-config";
 
-function catchAll( err:any, request: Request, response: Response, next: NextFunction ){
-    
+function catchAll(err: any, request: Request, response: Response, next: NextFunction) {
+
     // log the error:
     console.log(err);
 
-    
+
     // const status = err.status ? err.status : 500; // Ternary
     const status = err.status || 500; // Short circut
-    
-    // log to file:
-    if( status >= 500 ){
-        logger.errorsLogger( err.message, err );
-    }
+
+    // // log to file:
+    // if (status >= 500) {
+    //     logger.errorsLogger(err.message, err);
+    // }
 
     let msg = err.message;
 
-    if( appConfig.isProduction && status >= 500 ){
-        msg = "Some Error...";
-    }
+    // if (appConfig.isProduction && status >= 500) {
+    //     msg = "Some Error...";
+    // }
 
 
     // Response to client:
